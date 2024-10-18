cf0d407c3d34dbc811f373fcb409fbe3ca8ea515
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-10-18 11:48:31 +0600

    Create needed database schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,28 @@
 import { serial, text, integer, pgSchema } from "drizzle-orm/pg-core";
-import { memberSchemaData } from "./member";
 
 export const guildSchema = pgSchema("guild_schema");
 
 export const guildSchemaData = guildSchema.table("guilds", {
     id: serial("id").primaryKey(),
-    guild_id: text("guild_id").notNull(),
+    guild_id: text("guild_id").notNull().unique(),
     name: text("name").notNull(),
     totalMembers: integer("total_members").default(0),
     level: integer("level").default(1),
     motto: text("motto").default(""),
     icon: text("icon").default("")
+});
+
+export const memberSchema = pgSchema("member_schema");
+
+export const memberSchemaData = memberSchema.table("members", {
+    id: serial("id").primaryKey(),
+    user_id: text("user_id").notNull().unique(),
+    name: text("name").default(""),
+    username: text("username").notNull(),
+    level: integer("level").default(1),
+    about: text("about").default(""),
+    icon: text("icon").default(""),
+    guild_id: text("guild_id").references(() => guildSchemaData.guild_id)
 });
 
 export const guildToMembers = guildSchema.table(
