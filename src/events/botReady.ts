a3986d59a7541f31227ef3e92d4489da8cc7b019
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-10-18 14:58:25 +0600

    Update botReady.ts

--- src/events/botReady.ts
+++ src/events/botReady.ts
@@ -3,6 +3,6 @@ import { createEvent } from "seyfert";
 export default createEvent({
     data: { once: true, name: "botReady" },
     run(user, client) {
-        client.logger.info(`${user.username} is ready to go on adventures!`);   
+        client.logger.info(`${user.username} is ready to go on adventures!`);
     }
 });
