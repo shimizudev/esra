2bc8685af5b6939212f1777f1c11d4c36c83e225
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-10-18 11:52:11 +0600

    Fix middleware message

--- src/middlewares/owner-only.middleware.ts
+++ src/middlewares/owner-only.middleware.ts
@@ -1,6 +1,6 @@
 import { createMiddleware } from "seyfert";
 
 export const ownerOnlyMiddleware = createMiddleware<void>((middle) => {
-    if (middle.context.author.id !== middle.context.guild("cache")?.ownerId) middle.stop("Only developers can use this command");
+    if (middle.context.author.id !== middle.context.guild("cache")?.ownerId) middle.stop("Only guild owner can use this command");
     middle.next();
 });
