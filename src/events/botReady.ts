733480750674da36920ca7881f60375384e4045a
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-10-22 16:50:38 +0600

    Add activity and presence

--- src/events/botReady.ts
+++ src/events/botReady.ts
@@ -1,8 +1,15 @@
 import { createEvent } from "seyfert";
+import { ActivityType, PresenceUpdateStatus } from "seyfert/lib/types";
 
 export default createEvent({
     data: { once: true, name: "botReady" },
-    run(user, client) {
+    async run(user, client) {
+        await client.gateway.setPresence({
+            activities: [ { name: "on your adventure", type: ActivityType.Playing }, { name: "/help", type: ActivityType.Watching } ],
+            status: PresenceUpdateStatus.Online,
+            afk: false,
+            since: null
+        });
         client.logger.info(`${user.username} is ready to go on adventures!`);
     }
 });
